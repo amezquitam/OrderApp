7e3b3532032d1ee2c875119fff35678f256f1e11
amezquitam <amezquitamiguel7@gmail.com>
2024-03-26 15:42:44 -0500

    Fixing definition of findClientByEmail

--- src/main/java/ford/group/orderapp/repository/ClientRepository.java
+++ src/main/java/ford/group/orderapp/repository/ClientRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    List<Client> findClientsByEmail(String email);
+    Client findClientByEmail(String email);
 
     List<Client> findClientsByAddress(String address);
 

fd23c617cf609c0b5e45119d5dc03d1df684205f
amezquitam <amezquitamiguel7@gmail.com>
2024-03-31 14:27:09 -0500

    Adding ClientController

--- src/main/java/ford/group/orderapp/api/v1/ClientController.java
+++ src/main/java/ford/group/orderapp/api/v1/ClientController.java
@@ -1,14 +1,17 @@
 package ford.group.orderapp.api.v1;
 
 import ford.group.orderapp.dto.client.ClientDTO;
+import ford.group.orderapp.exception.ClientNotFoundException;
 import ford.group.orderapp.service.ClientService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController("/api/v1/client")
+@RestController
+@RequestMapping("/api/v1/client")
 public class ClientController {
 
     ClientService clientService;
@@ -19,8 +22,12 @@ public class ClientController {
 
     @GetMapping("/{id}")
     ResponseEntity<ClientDTO> get(@PathVariable("id") Long clientId) {
-        var client = clientService.findClientById(clientId);
-        return ResponseEntity.ok(client);
+        try {
+            var client = clientService.findClientById(clientId);
+            return ResponseEntity.ok(client);
+        } catch (ClientNotFoundException err) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }

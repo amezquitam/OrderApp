0341844f71ef677f2c364d7f44199c33eeeb8edc
amezquitam <amezquitamiguel7@gmail.com>
2024-04-02 12:03:03 -0500

    fixing OrderMapper

--- src/main/java/ford/group/orderapp/dto/order/OrderMapper.java
+++ src/main/java/ford/group/orderapp/dto/order/OrderMapper.java
@@ -2,10 +2,14 @@ package ford.group.orderapp.dto.order;
 
 import ford.group.orderapp.entities.Order;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
+    @Mapping(source = "order.status", target = "orderStatus")
     OrderDTO orderToOrderDTO(Order order);
+    @Mapping(source = "orderDTO.orderStatus", target = "status")
     Order orderDTOToOrder(OrderDTO orderDTO);
+    @Mapping(source = "orderToSaveDTO.orderStatus", target = "status")
     Order orderSaveDTOToOrder(OrderToSaveDTO orderToSaveDTO);
 }

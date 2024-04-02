96b9953679241f2f6b7ceb051297805d68e53bfa
amezquitam <amezquitamiguel7@gmail.com>
2024-04-01 22:31:23 -0500

    updating client controller

--- src/main/java/ford/group/orderapp/api/v1/ClientController.java
+++ src/main/java/ford/group/orderapp/api/v1/ClientController.java
@@ -1,17 +1,17 @@
 package ford.group.orderapp.api.v1;
 
 import ford.group.orderapp.dto.client.ClientDTO;
+import ford.group.orderapp.dto.client.ClientToSaveDTO;
 import ford.group.orderapp.exception.ClientNotFoundException;
 import ford.group.orderapp.service.ClientService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/v1/client")
+@RequestMapping("/api/v1/customers")
 public class ClientController {
 
     ClientService clientService;
@@ -20,11 +20,39 @@ public class ClientController {
         this.clientService = clientService;
     }
 
+    @GetMapping("/")
+    ResponseEntity<List<ClientDTO>> index() {
+        List<ClientDTO> clients = clientService.findAll();
+        return ResponseEntity.ok(clients);
+    }
+
     @GetMapping("/{id}")
     ResponseEntity<ClientDTO> get(@PathVariable("id") Long clientId) {
         try {
             var client = clientService.findClientById(clientId);
             return ResponseEntity.ok(client);
+        } catch (ClientNotFoundException err) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/")
+    ResponseEntity<ClientDTO> create(@RequestBody ClientToSaveDTO clientToSave) {
+        // validations
+
+        // save
+        ClientDTO savedClient = clientService.saveClient(clientToSave);
+
+        return ResponseEntity.ok(savedClient);
+    }
+
+    @GetMapping("/email/{email}")
+    ResponseEntity<ClientDTO> findByEmail(@PathVariable("email") String email) {
+        try {
+            ClientDTO client = clientService.findClientByEmail(email);
+
+            return ResponseEntity.ok(client);
+
         } catch (ClientNotFoundException err) {
             return ResponseEntity.notFound().build();
         }
